1031e6303b9389341fb14b9c4c6a8c106f800b16
大萝北子 <37783752+Hxmg@users.noreply.github.com>
2020-11-09 08:54:25 +0800

    Update wrapper.js

--- source/wrapper.js
+++ source/wrapper.js
@@ -31,14 +31,10 @@
 var emitter = events.emitter(threads.currentThread());
 threads.start(function() {
   emitter.emit("evaluate", (function(){
-    var resp = http.get("https://gitee.com/stageguard/SkyAutoPlayerScript/raw/master/source/SkyAutoplayer.js");
+    var resp = http.get("https://raw.githubusercontent.com/Hxmg/SkyAutoPlayerScript/master/source/SkyAutoplayer.js");
     if(resp.statusCode >= 200 && resp.statusCode < 300) {
       return resp.body.string();
     } else {
-      resp = http.get("https://cdn.jsdelivr.net/gh/StageGuard/SkyAutoPlayerScript@" + http.get("https://gitee.com/stageguard/SkyAutoPlayerScript/raw/master/gitVersion").body.string() + "/source/SkyAutoplayer.js");
-      if(resp.statusCode >= 200 && resp.statusCode < 300) {
-        return resp.body.string();
-      } else {
         return "console.show();console.log(\"Failed to load script\")";
       }
 	}
