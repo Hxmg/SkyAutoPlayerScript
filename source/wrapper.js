9fddd204497df0200deafabee9cdca80f53a8219
大萝北子 <37783752+Hxmg@users.noreply.github.com>
2020-11-09 08:59:02 +0800

    Update wrapper.js

--- source/wrapper.js
+++ source/wrapper.js
@@ -11,17 +11,14 @@
 	  (Weibo@StageGuard)
 	  (CoolApk@StageGuard)
 	  (Twiter@stageguardcn)
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
@@ -31,12 +28,11 @@
 var emitter = events.emitter(threads.currentThread());
 threads.start(function() {
   emitter.emit("evaluate", (function(){
-    var resp = http.get("https://raw.githubusercontent.com/Hxmg/SkyAutoPlayerScript/master/source/SkyAutoplayer.js");
+    var resp = http.get("https://gitee.com/stageguard/SkyAutoPlayerScript/raw/master/source/SkyAutoplayer.js");
     if(resp.statusCode >= 200 && resp.statusCode < 300) {
       return resp.body.string();
     } else {
         return "console.show();console.log(\"Failed to load script\")";
-      }
 	}
   }()));
 });
